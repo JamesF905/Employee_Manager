5b043a0bb9dd278027416bcf38ae9aa417115baf
James <jameslfidlin@gmail.com>
2022-07-21 18:50:59 -0400

    converted db connection to use env

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,15 @@
 const mySQL = require('mysql2'); // import mysql
+require('dotenv').config(); // import in order to use dotenv to pull database credentials from the env file
 const db = mySQL.createConnection({
     host: "localhost",
-    user: "root",
-    password: "Temppass888!",
-    database: "employee_manager_db"
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
     
 });
 
 db.connect(function (err) {
-    err ?  console.log(err) : console.log('Connected to Database!');
+    err ? console.log(err) : console.log(`Connected to Database!`);
 });
 
 module.exports = db;
